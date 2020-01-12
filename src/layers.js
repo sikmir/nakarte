c9f220c8b553d030c94e606bb1b2107b38089105
Nikolay Korotkiy <sikmir@gmail.com>
2020-01-13 02:49:44 +0300

    layers: use thunderforestKey

--- src/layers.js
+++ src/layers.js
@@ -479,8 +479,8 @@ class LayerGroupWithOptions extends L.LayerGroup {
                     isDefault: true,
                     layer: new RetinaTileLayer(
                         [
-                            'https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png',
-                            'https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}@2x.png',
+                            `https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=${config.thunderforestKey}`, // eslint-disable-line max-len
+                            `https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}@2x.png?apikey=${config.thunderforestKey}`, // eslint-disable-line max-len
                         ],
                         {
                             code: 'Ocm',
@@ -498,8 +498,8 @@ class LayerGroupWithOptions extends L.LayerGroup {
                     isDefault: true,
                     layer: new RetinaTileLayer(
                         [
-                            'https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png',
-                            'https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}@2x.png',
+                            `https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=${config.thunderforestKey}`, // eslint-disable-line max-len
+                            `https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}@2x.png?apikey=${config.thunderforestKey}`, // eslint-disable-line max-len
                         ],
                         {
                             code: 'Oso',
