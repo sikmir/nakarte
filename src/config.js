ab8a6a9e899c701f7d658cccd4d969bbb59ddb5f
Nikolay Korotkiy <sikmir@gmail.com>
2020-01-13 03:09:17 +0300

    Update config

--- src/config.js
+++ src/config.js
@@ -2,19 +2,17 @@ import secrets from './secrets';
 
 const config = {
     caption: `
-        <a href="https://docs.nakarte.me">Documentation</a> |
-        <a href="https://about.nakarte.me">News</a> |
-        <a href="mailto:nakarte@nakarte.me" target="_self">nakarte@nakarte.me</a> |
-        <a href="https://about.nakarte.me/p/blog-page_29.html">Donate</a>`,
-    defaultLocation: [49.73868, 33.45886],
-    defaultZoom: 8,
+       <a href="https://sikmir.ru">sikmir.ru</a> |
+       <a href="https://tiles.sikmir.ru">tiles.sikmir.ru</a>`,
+    defaultLocation: [60.0, 30.0],
+    defaultZoom: 7,
     googleApiUrl: `https://maps.googleapis.com/maps/api/js?v=3&key=${secrets.google}`,
     westraDataBaseUrl: 'https://nakarte.me/westraPasses/',
     CORSProxyUrl: 'https://proxy.nakarte.me/',
     elevationsServer: 'https://elevation.nakarte.me/',
     wikimediaCommonsCoverageUrl: 'https://tiles.nakarte.me/wikimedia_commons_images/{z}/{x}/{y}',
     geocachingSuUrl: 'https://nakarte.me/geocachingSu/geocaching_su2.json',
-    tracksStorageServer: 'https://tracks.nakarte.me',
+    tracksStorageServer: 'https://tracks.sikmir.ru',
     wikimapiaTilesBaseUrl: 'https://proxy.nakarte.me/wikimapia/',
     mapillaryRasterTilesUrl: 'https://mapillary.nakarte.me/{z}/{x}/{y}',
     urlsBypassCORSProxy: [new RegExp('^https://pkk\\.rosreestr\\.ru/', 'u')],
