f8db220d80555ab75bb9950590d510a7c92c9ab6
Nikolay Korotkiy <sikmir@gmail.com>
2020-07-12 04:09:52 +0300

    layers: add extremum tracks

--- src/layers.js
+++ src/layers.js
@@ -1228,6 +1228,22 @@ class LayerGroupWithOptions extends L.LayerGroup {
                         }
                     )
                 },
+                {
+                    title: 'Extremum tracks',
+                    isDefault: true,
+                    layer: L.tileLayer('https://layers.extremum.org/v2/selector/tracks/{z}/{x}/{y}.png?' +
+                                       'layers=Extremum/LizaAlert/Otklik/Other',
+                        {
+                            code: 'aT',
+                            isOverlay: true,
+                            isOverlayTransparent: true,
+                            scaleDependent: true,
+                            print: true,
+                            jnx: false,
+                            shortName: 'extremum_tracks'
+                        }
+                    )
+                },
     ];
 
     const groupsDefs = [
@@ -1312,6 +1328,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
                 'Strava heatmap (run)',
                 'Strava heatmap (ride)',
                 'Strava heatmap (winter)',
+                'Extremum tracks',
             ],
 
         },
@@ -1399,6 +1416,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
         'Soviet topo maps grid',
         'Wikimapia',
         'OpenRailwayMap',
+        'Extremum tracks',
 
         // point overlays
         'Mountain passes (Westra)',
