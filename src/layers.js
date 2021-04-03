d039a9454568cadee3bd1bc08047fc8b2a0b75d2
Nikolay Korotkiy <sikmir@gmail.com>
2021-04-03 22:01:22 +0300

    layers: add Inkatlas

--- src/layers.js
+++ src/layers.js
@@ -1109,7 +1109,21 @@ class LayerGroupWithOptions extends L.LayerGroup {
                         }
                     )
                 },
-
+                {
+                    title: 'Inkatlas Outdoor',
+                    isDefault: true,
+                    layer: L.tileLayer('https://tile.inkatlas.com/inkatlas/outdoor/{z}/{x}/{y}.png',
+                        {
+                            code: 'Ink',
+                            isOverlay: false,
+                            scaleDependent: true,
+                            print: true,
+                            jnx: true,
+                            shortName: 'inkatlas',
+                            attribution: '<a href="https://inkatlas.com/">Inkatlas</a>',
+                        }
+                    )
+                },
     ];
 
     const groupsDefs = [
@@ -1152,7 +1166,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
                 'OSM Outdoors',
                 'mapy.cz tourist',
                 'mapy.cz winter',
-
+                'Inkatlas Outdoor',
             ],
         },
         {
@@ -1213,6 +1227,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
         'OSM Outdoors',
         'mapy.cz tourist',
         'mapy.cz winter',
+        'Inkatlas Outdoor',
         // Satellite
         'ESRI Satellite',
         'Yandex Satellite',
