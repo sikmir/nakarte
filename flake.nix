869b6c342a142ed1cbbf035b580c2db17673db54
Nikolay Korotkiy <sikmir@gmail.com>
2021-08-14 20:43:15 +0300

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "nakarte.me";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          devShell = with pkgs; mkShell {
+            buildInputs = [ yarn ];
+          };
+        }
+      );
+}
