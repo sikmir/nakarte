469d8454d4773a07386a2ce49a7824d44e3a5bcc
Nikolay Korotkiy <sikmir@gmail.com>
2021-08-14 19:53:01 +0300

    layers: add my tracks (vector)

--- src/layers.js
+++ src/layers.js
@@ -1282,6 +1282,21 @@ class LayerGroupWithOptions extends L.LayerGroup {
                         }
                     )
                 },
+                {
+                    title: 'Tracks by @sikmir (vector)',
+                    isDefault: true,
+                    layer: new L.Layer.MyTracksVector(
+                        {
+                            code: 'Mytv',
+                            isOverlay: true,
+                            isOverlayTransparent: true,
+                            scaleDependent: true,
+                            print: false,
+                            jnx: false,
+                            shortName: 'my_vtracks'
+                        }
+                    )
+                },
     ];
 
     const groupsDefs = [
@@ -1369,6 +1384,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
                 'Strava heatmap (winter)',
                 'Extremum tracks',
                 'Tracks by @sikmir (raster)',
+                'Tracks by @sikmir (vector)',
             ],
 
         },
@@ -1459,6 +1475,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
         'OpenRailwayMap',
         'Extremum tracks',
         'Tracks by @sikmir (raster)',
+        'Tracks by @sikmir (vector)',
 
         // point overlays
         'Mountain passes (Westra)',
