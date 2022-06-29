5a0cf100c23d539ff7db5a89ad0b8cff231e73cd
Nikolay Korotkiy <sikmir@disroot.org>
2022-06-30 02:43:20 +0300

    layers: add OpenGeofiction

--- src/layers.js
+++ src/layers.js
@@ -1235,6 +1235,22 @@ class LayerGroupWithOptions extends L.LayerGroup {
                     )
                 },
                 {
+                    title: 'OpenGeofiction',
+                    isDefault: false,
+                    layer: L.tileLayer('https://tile.opengeofiction.net/ogf-carto/{z}/{x}/{y}.png',
+                        {
+                            code: 'Ogf',
+                            isOverlay: false,
+                            scaleDependent: true,
+                            print: true,
+                            jnx: true,
+                            shortName: 'ogf',
+                            attribution: '<a href="https://opengeofiction.net/copyright">' +
+                                '&copy; OpenGeofiction contributors</a>',
+                        }
+                    )
+                },
+                {
                     title: 'Sentinel-2 L1C',
                     isDefault: true,
                     layer: L.tileLayer.wms(
@@ -1343,6 +1359,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
                 'mapy.cz winter',
                 'Inkatlas Outdoor',
                 'ÖPNVKarte',
+                'OpenGeofiction',
             ],
         },
         {
@@ -1413,6 +1430,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
         'mapy.cz winter',
         'Inkatlas Outdoor',
         'ÖPNVKarte',
+        'OpenGeofiction',
         // Satellite
         'ESRI Satellite',
         'Yandex Satellite',
