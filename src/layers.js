29ff635367976a1822fe0f23544c92a4f26e0f87
Nikolay Korotkiy <sikmir@disroot.org>
2025-04-10 18:06:48 +0400

    layers: add OpenTopoMap.cz

--- src/layers.js
+++ src/layers.js
@@ -476,6 +476,23 @@ class LayerGroupWithOptions extends L.LayerGroup {
                     )
                 },
                 {
+                    title: 'OpenTopoMap.cz',
+                    isDefault: true,
+                    layer: L.tileLayer('https://tile-{s}.opentopomap.cz/{z}/{x}/{y}.png',
+                        {
+                            code: 'Otmcz',
+                            isOverlay: false,
+                            maxNativeZoom: 18,
+                            scaleDependent: true,
+                            print: true,
+                            jnx: true,
+                            noCors: false,
+                            shortName: 'opentopocz',
+                            attribution: '<a href="https://opentopomap.cz/">OpenTopoMap.cz</a>',
+                        }
+                    )
+                },
+                {
                     title: 'OpenRailwayMap',
                     isDefault: true,
                     layer: L.tileLayer('http://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png',
@@ -1371,6 +1388,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
             title: 'OpenStreetMap alternatives',
             layers: [
                 'OpenTopoMap',
+                'OpenTopoMap.cz',
                 'OpenCycleMap',
                 'OSM Outdoors',
                 'mapy.cz tourist',
@@ -1442,6 +1460,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
         'OpenStreetMap',
         'CyclOSM',
         'OpenTopoMap',
+        'OpenTopoMap.cz',
         'OpenCycleMap',
         'OSM Outdoors',
         'mapy.cz tourist',
